e133ae47bebaf8e240ddd8d985f901e71a5338eb
fccJashCoda <personalrevolution2014@gmail.com>
2021-02-26 00:55:01 +0100

    Add: delete product button/function

--- product.js
+++ product.js
@@ -6,7 +6,22 @@
   shared.getProduct(product_id).then(showProduct);
 
   function showProduct(product) {
-    const buttons = `<a href="/edit.html?id=${product.id}" class="btn btn-success">Edit Product</a>`;
+    const buttons = `
+    <a href="/edit.html?id=${product.id}" class="btn btn-success">Edit Product</a>
+    <button id="delete" class="btn btn-danger">Delete Product</button>`;
     shared.addProductToPage(product, 12, productSection, buttons);
+
+    document.getElementById('delete').addEventListener('click', () => {
+      deleteProduct(product_id).then(() => {
+        window.location = '/';
+      });
+    });
+  }
+
+  function deleteProduct(id) {
+    const options = {
+      method: 'DELETE',
+    };
+    return fetch(`${shared.API_URL}/${id}`, options);
   }
 })();
